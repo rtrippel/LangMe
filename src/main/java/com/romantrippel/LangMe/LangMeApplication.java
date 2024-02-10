2f5f1e8fd9d74ed6574883cd0d3c8e4e270d8f9b
Roman Trippel <rtrippel84@gmail.com>
2024-02-10 20:29:36 +0100

    first commit

---
+++ src/main/java/com/romantrippel/LangMe/LangMeApplication.java
@@ -0,0 +1,13 @@
+package com.romantrippel.LangMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LangMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romantrippel/LangMe/LangMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romantrippel.LangMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
